d868896edbccadd633d232df7a8c877f4f73f007
KingKai-04 <Laughingtoad417@gmail.com>
2022-09-30 10:15:27 -0400

    Daily Progress
    Properly imported images and begun making the card images.

---
+++ matching-game/src/App.js
@@ -0,0 +1,107 @@
+import './App.css';
+import images from './components/images'
+
+function App() {
+  return (
+    <div className="board">
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+      <div className="card-outer">
+        <div className="card">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ matching-game/src/components/images.js
@@ -0,0 +1,12 @@
+import img01 from './cardimg/img-1.png';
+import img02 from './cardimg/img-2.png';
+import img03 from './cardimg/img-3.png';
+import img04 from './cardimg/img-4.png';
+import img05 from './cardimg/img-5.png';
+import img06 from './cardimg/img-6.png';
+import img07 from './cardimg/img-7.png';
+import img08 from './cardimg/img-8.png';
+
+const Images = [img01, img02, img03, img04, img05, img06, img07, img08]
+
+export default Images 
